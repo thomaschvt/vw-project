7e1d87fc925ba76c947785e4c5dd95ea41ac7a30
thomaschvt <thomaschvt@gmail.com>
2013-12-14 18:03:10 +0100

    ajout du bundle Front

---
+++ src/VintageWest/FrontBundle/Resources/public/js/main.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function() {
+       
+    $('#coin-slider').coinslider({
+        width: 755, 
+        height: 320,
+        navigation: true, 
+        delay: 5000
+    });
+    
+    $( "#accordion" ).accordion();
+});
